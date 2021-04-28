3382bfff62025a2615707818f287a7124aa9a46f
Vernon Wright <vhcwright@gmail.com>
2021-04-28 09:45:07 -0400

    Finalize renderer boilerplate

--- src/Renderer.java
+++ src/Renderer.java
@@ -25,9 +25,18 @@ public class Renderer extends JFrame {
 
         Graphics g = bs.getDrawGraphics();
 
-        g.setColor(Color.black);
-        g.fillRect(0, 0, s.getScreenWidth() / 2, s.getScreenHeight() / 2);
+        BufferedImage bi = new BufferedImage(s.getScreenWidth(), s.getScreenHeight(), BufferedImage.TYPE_INT_ARGB);
+        for (int x = 0; x < bi.getWidth(); x++) {
+            for (int y = 0; y < bi.getHeight(); y++) {
+                if (x > bi.getWidth() / 2 && y < bi.getHeight() / 2) {
+                    bi.setRGB(x, y, new Color(255, 0, 255).getRGB());
+                } else {
+                    bi.setRGB(x, y, new Color(0, 0, 0).getRGB());
+                }
+            }
+        }
 
+        g.drawImage(bi, 0, 0, null);
         g.dispose();
         bs.show();
 
